716a50a3c6217c6149ffd2805786ad8dd106be90
river <refractionisaneatconcept@gmail.com>
2026-08-06 04:00:12 -0400

    switch condition

---
+++ src/main/java/pro/fazeclan/river/jarona/condition/SwitchCondition.java
@@ -0,0 +1,39 @@
+package pro.fazeclan.river.jarona.condition;
+
+import java.util.UUID;
+import java.util.function.Function;
+
+public class SwitchCondition extends Condition {
+
+    final boolean initial;
+    boolean on;
+
+    public SwitchCondition(boolean on, Function<Condition, String> hud, UUID playerUUID) {
+        this.on = on;
+        this.initial = on;
+        setHud(hud);
+        this.playerUUID = playerUUID;
+    }
+
+    public SwitchCondition(boolean on, UUID playerUUID) {
+        this.on = on;
+        this.initial = on;
+        this.playerUUID = playerUUID;
+    }
+
+    public SwitchCondition(boolean on) {
+        this.on = on;
+        this.initial = on;
+    }
+
+    @Override
+    public boolean getAvailable() {
+        return on;
+    }
+
+    @Override
+    public void reset() {
+        on = initial;
+    }
+
+}
