bb89aded0d2381297f086fd3e378931c55f1cbd9
river <refractionisaneatconcept@gmail.com>
2026-08-17 02:08:43 -0400

    feat: statistic lookup

--- src/main/java/pro/fazeclan/river/jarona/command/StatisticCommand.java
+++ src/main/java/pro/fazeclan/river/jarona/command/StatisticCommand.java
@@ -4,6 +4,9 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.resolvers.PlayerProfileListResolver;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import pro.fazeclan.river.jarona.Jarona;
@@ -17,15 +20,48 @@ public class StatisticCommand {
         return Commands.literal("statistic")
                 .then(
                         Commands.argument("game", new GameArgument())
+                                .then(Commands.argument("player", ArgumentTypes.playerProfiles())
+                                        .executes(ctx -> {
+                                            var source = ctx.getSource().getSender();
+                                            var plugin = Jarona.getInstance();
+                                            var manager = plugin.getStatisticManager();
+                                            var game = ctx.getArgument("game", Game.class);
+                                            var profiles = ctx.getArgument("player", PlayerProfileListResolver.class).resolve(ctx.getSource());
+                                            var mm = MiniMessage.miniMessage();
+
+                                            for (var profile : profiles) {
+                                                if (profile.getId() == null) {
+                                                    continue;
+                                                }
+                                                source.sendMessage(ServerUtil.formatComponent(
+                                                        "<green>" + profile.getName() + "'s statistics on " + game.getName() + "!<newline><red>[Note: This may take a bit to fetch!]</red></green>"
+                                                ));
+                                                manager.getAllStats(profile.getId(), game.getKey()).thenAccept(map -> {
+                                                    for (var entry : map.entrySet()) {
+                                                        var display = game.getStatDefinitions().stream()
+                                                                .filter(definition -> definition.key().equals(entry.getKey()))
+                                                                .findFirst().orElse(null);
+                                                        Bukkit.getScheduler().runTask(plugin, () -> {
+                                                            assert display != null;
+                                                            source.sendMessage(mm.deserialize("<yellow>" + display.display() + ": " + entry.getValue() + "</yellow>"));
+                                                        });
+                                                    }
+                                                });
+                                            }
+
+                                            return Command.SINGLE_SUCCESS;
+                                        })
+                                )
                                 .executes(ctx -> {
                                     if (!(ctx.getSource().getSender() instanceof Player player)) return Command.SINGLE_SUCCESS;
 
                                     var plugin = Jarona.getInstance();
                                     var manager = plugin.getStatisticManager();
                                     var game = ctx.getArgument("game", Game.class);
+                                    var mm = MiniMessage.miniMessage();
 
                                     player.sendMessage(ServerUtil.formatComponent(
-                                            "<green>Your statistics on " + game.getName() + "! <red>[Note: This may take a bit to fetch!]</red></green>"
+                                            "<green>Your statistics on " + game.getName() + "!<newline><red>[Note: This may take a bit to fetch!]</red></green>"
                                     ));
                                     manager.getAllStats(player.getUniqueId(), game.getKey()).thenAccept(map -> {
                                         for (var entry : map.entrySet()) {
@@ -34,9 +70,7 @@ public class StatisticCommand {
                                                     .findFirst().orElse(null);
                                             Bukkit.getScheduler().runTask(plugin, () -> {
                                                 assert display != null;
-                                                player.sendMessage(ServerUtil.formatComponent(
-                                                        "<yellow>" + display.display() + ": " + entry.getValue() + "</yellow>"
-                                                ));
+                                                player.sendMessage(mm.deserialize("<yellow>" + display.display() + ": " + entry.getValue() + "</yellow>"));
                                             });
                                         }
                                     });
